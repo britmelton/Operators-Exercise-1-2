c6bb4da6493e49586b42ab6b19f3468c62be4fb0
brittany melton <brittanymelton26@gmail.com>
2022-02-28 16:19:09 -0500

    added instruction comments for class exercise.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,6 +6,18 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
+            #region Class Exercise Instructions
+            //In class Exercise 
+            /* Create a simple program to write out the results of addition, subtraction, multiplication, division operations.
+             * For division, create two integer variables called a and b. 
+             * Create a third integer variable and named quotient (the result of a division) that stores the result of the division of a and b.
+             * Create another integer variable named remainder that stores the remainder (using the % operator). 
+             * Write out the results using Console.WriteLine or Console.Write
+             * Write out the results in the following form: if a = 17 and b = 4, print the following:	
+                17/4 is 4 remainder 1	*/
+            #endregion
+
+
             Console.WriteLine("Enter first number:");
             var c = double.Parse(Console.ReadLine());
             Console.WriteLine("\nEnter second number:");
@@ -20,32 +32,14 @@ namespace OperatorExercise
             var product = c * d;
             Console.WriteLine($"the product of {c}*{d} is {product}");
 
+            Console.WriteLine("Enter a number: ");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second number: ");
+            var b = double.Parse(Console.ReadLine());
+            var quotient = a / b;
+            var remainder = a % b;
 
-            int a = 17;
-            int b = 4;
-            int quotient = a / b;
-            int remainder = a % b;
-
-
-            if (a == 17 && b == 4)
-                Console.WriteLine($"\n{a}/{b} is {quotient} remainder {remainder}");
-
-
-
-            Console.WriteLine("\n\nwhat is the radius of your circle?");
-            double radius = double.Parse(Console.ReadLine());
-            double area = AreaOfCircle(radius);
-            Console.WriteLine($"\nThe area of a circle with radius of {radius} is {area}");
-
-        }
-
-        public static double AreaOfCircle(double radius)
-        {
-            double pi = Math.PI;
-
-            return pi * radius * radius;
-
-        }
-
+            Console.WriteLine($"\n{a}/{b} is {quotient} remainder {remainder}");
+        } 
     }
 }   
